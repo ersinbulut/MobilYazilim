740ac34a223fe68722de4907ffe494402d06804a
ersinbulut <ersinblt61@gmail.com>
2021-09-01 18:22:52 +0300

    String Birleştirmesi

---
+++ Udemy/Degiskenler/app/src/main/java/com/example/degiskenler/StringBirlestirme.java
@@ -0,0 +1,31 @@
+package com.example.degiskenler;
+
+public class StringBirlestirme {
+    public static void main(String args[]){
+
+        System.out.println("----------------------");
+        String yazi="Mehmet";
+
+        String sonuc=yazi+"merhaba";
+
+        String str="Kişinin adı: "+"Mehmet";
+        int not=80;
+        String ders="Matematik";
+        String ogrenciAdi="Zeynep ";
+        String metin=ogrenciAdi+ders+" sınavından "+not+" aldı";
+        int sayi1=50;
+        int sayi2=40;
+
+        System.out.println("Sayı 1: "+sayi1);
+        System.out.println("Sayı 2: "+sayi2);
+        System.out.println("Toplam : "+(sayi1+sayi2));
+
+
+        /*System.out.println(sonuc);
+        System.out.println(str);
+        System.out.println(metin);
+*/
+
+        System.out.println("----------------------");
+    }
+}
