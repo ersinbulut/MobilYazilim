6defc47b0b240aa57d20aa6629ccfa5e99cba17d
ersinbulut <ersinblt61@gmail.com>
2021-09-02 23:38:30 +0300

    Metodların Aşırı Yüklenmesi ( Overloading )

---
+++ Udemy/NesneTabanliProgramlama/app/src/main/java/com/example/nesnetabanliprogramlama/MatematikselHesaplama.java
@@ -0,0 +1,20 @@
+package com.example.nesnetabanliprogramlama;
+
+public class MatematikselHesaplama {
+
+    public void topla(int s1,int s2){
+        System.out.println("Toplam : "+(s1+s2));
+    }
+    public void topla(double s1,int s2){
+        System.out.println("Toplam : "+(s1+s2));
+    }
+    public void topla(int s1,double s2){
+        System.out.println("Toplam : "+(s1+s2));
+    }
+    public void topla(int s1,int s2,String isim){
+        System.out.println("Toplam : "+(s1+s2)+" : "+isim);
+    }
+    public void topla(double s1,double s2){
+        System.out.println("Toplam : "+(s1+s2));
+    }
+}
---
+++ Udemy/NesneTabanliProgramlama/app/src/main/java/com/example/nesnetabanliprogramlama/OverloadingMain.java
@@ -0,0 +1,10 @@
+package com.example.nesnetabanliprogramlama;
+
+public class OverloadingMain {
+    public static void main(String args[]){
+        MatematikselHesaplama mh=new MatematikselHesaplama();
+        mh.topla(4.59,2);
+
+        mh.topla(40,30,"mehmet");
+    }
+}
