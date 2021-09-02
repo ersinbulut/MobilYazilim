87fcf3f795c9341be8647fd57bc8b0db1ff3cd65
ersinbulut <ersinblt61@gmail.com>
2021-09-02 13:26:55 +0300

    Karşılaştırma Operatörleri

---
+++ Udemy/StandartProgramlamaYapilari/app/src/main/java/com/example/standartprogramlamayapilari/KarsilastirmaOperatoru.java
@@ -0,0 +1,28 @@
+package com.example.standartprogramlamayapilari;
+
+public class KarsilastirmaOperatoru {
+
+    public static void main(String args[]){
+
+        int s1=60;
+        int s2=50;
+
+        int y1=90;
+        int y2=80;
+
+        //boolean kontrol=s1==s2;
+
+        /*System.out.println(s1>s2);
+        System.out.println(s1>=s2);
+        System.out.println(s1<s2);
+        System.out.println(s1<=s2);
+        System.out.println(s1!=s2);*/
+
+       // System.out.println(s1>s2 && y1>y2); //ve her iki şartta sağlanırsa true
+
+        System.out.println(s1>s2 || y1>y2); //veya en az bir şart sağlanırsa true, her ikiside sağlanmazsa false
+
+
+
+    }
+}
