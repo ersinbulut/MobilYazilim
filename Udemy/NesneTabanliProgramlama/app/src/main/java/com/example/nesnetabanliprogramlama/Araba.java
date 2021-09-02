cf8a80cc158b20ff6555c5d2709f75bc682f913f
ersinbulut <ersinblt61@gmail.com>
2021-09-02 20:03:49 +0300

    Araba Analojisi

--- Udemy/NesneTabanliProgramlama/app/src/main/java/com/example/nesnetabanliprogramlama/Araba.java
+++ Udemy/NesneTabanliProgramlama/app/src/main/java/com/example/nesnetabanliprogramlama/Araba.java
@@ -5,4 +5,22 @@ public class Araba {
     int hız;
     double uzunluk;
     boolean calisiyorMu;
+
+    //method davranışı
+    public void calistir(){
+        calisiyorMu=true;
+    }
+    public void durdur(){
+        calisiyorMu=false;
+    }
+    public void hizlan(int miktar){
+        hız= hız+miktar;
+    }
+
+    public void yavasla(int miktar){
+        hız= hız-miktar;
+    }
+
+
+
 }
--- Udemy/NesneTabanliProgramlama/app/src/main/java/com/example/nesnetabanliprogramlama/NesneTabanliGiris.java
+++ Udemy/NesneTabanliProgramlama/app/src/main/java/com/example/nesnetabanliprogramlama/NesneTabanliGiris.java
@@ -17,8 +17,9 @@ public class NesneTabanliGiris {
         tofas.hız=100;
         tofas.uzunluk=2.47;
         tofas.calisiyorMu=false;
+
         System.out.println("----------------");
-        System.out.println("BMW");
+     /*   System.out.println("BMW");
         System.out.println("----------------");
         System.out.println(bmw.renk);
         System.out.println(bmw.hız);
@@ -30,8 +31,36 @@ public class NesneTabanliGiris {
         System.out.println(tofas.renk);
         System.out.println(tofas.hız);
         System.out.println(tofas.uzunluk);
-        System.out.println(tofas.calisiyorMu);
+        System.out.println(tofas.calisiyorMu);*/
         System.out.println("----------------");
 
+        Araba reno = new Araba();
+        reno.renk="Sarı";
+        reno.hız=150;
+        reno.uzunluk=3.67;
+        reno.calisiyorMu=true;
+
+     /*   System.out.println("bmw çalışma:"+bmw.calisiyorMu);
+        System.out.println("tofas çalışma:"+tofas.calisiyorMu);
+        System.out.println("reno çalışma:"+reno.calisiyorMu);
+        System.out.println("----------------");
+        tofas.calistir();
+        reno.durdur();
+        reno.hizlan(30);
+
+
+        System.out.println("bmw çalışma:"+bmw.calisiyorMu);
+        System.out.println("tofas çalışma:"+tofas.calisiyorMu);
+        System.out.println("reno çalışma:"+reno.calisiyorMu);*/
+
+        System.out.println(bmw.hız);
+        bmw.hizlan(30);
+        System.out.println(bmw.hız);
+        bmw.yavasla(90);
+        System.out.println(bmw.hız);
+        System.out.println("---------------");
+        System.out.println(tofas.hız);
+        tofas.hizlan(100);
+        System.out.println(tofas.hız);
     }
 }
