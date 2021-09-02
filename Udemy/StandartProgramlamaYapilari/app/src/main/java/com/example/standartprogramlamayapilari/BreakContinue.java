b796ba7c938f6230252ee109599184b91d661457
ersinbulut <ersinblt61@gmail.com>
2021-09-02 16:20:55 +0300

    Break ve Continue

---
+++ Udemy/StandartProgramlamaYapilari/app/src/main/java/com/example/standartprogramlamayapilari/BreakContinue.java
@@ -0,0 +1,16 @@
+package com.example.standartprogramlamayapilari;
+
+public class BreakContinue {
+    public static void main(String args[]) {
+        //0-5 ->6 kere
+        for (int i = 0; i<6;i++){
+            System.out.println("Index : "+i);
+            if (i == 3){
+                continue;
+                //break;
+            }
+
+        }
+
+    }
+}
